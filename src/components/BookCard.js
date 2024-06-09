951d35569dee3bac3ae93f608f028623c7a1a379
Amey Sawalkar <ameykishor@gmail.com>
2024-06-09 14:42:43 +0530

    final

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -5,7 +5,7 @@ const BookCard = ({ book, onAdd }) => {
     <div
       key={book.key}
       className="flex flex-col justify-around border-2 border-gray-600 rounded-lg p-3 m-3
-      w-[90%] sm:w-[40%]"
+      w-[90%] sm:w-[30%]"
     >
       <h3>
         <span className="font-semibold">Title:</span> {book.title}
@@ -17,7 +17,7 @@ const BookCard = ({ book, onAdd }) => {
         <span className="font-semibold">Edition Count:</span>{" "}
         {book.edition_count}
       </p>
-      <button onClick={() => onAdd(book)} className="w-[70%] lg:w-[50%] bg-blue-400 text-white font-semibold px-1 rounded-md hover:bg-blue-500 mx-auto my-3">Add to Bookshelf</button>
+      <button onClick={() => onAdd(book)} className="w-[70%] lg:w-[50%] bg-blue-400 text-white font-semibold p-1 rounded-md hover:bg-blue-500 mt-3">Add to Bookshelf</button>
     </div>
   );
 };
