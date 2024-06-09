9dda76fa5c76c1ac6627a0c4f9a3ace57341a23f
Amey Sawalkar <ameykishor@gmail.com>
2024-06-09 15:52:26 +0530

    status position changed to fixed

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -6,8 +6,8 @@ const BookSearch = () => {
   const [query, setQuery] = useState("");
   const [books, setBooks] = useState([]);
   const [searching, setSearching] = useState(false);
-  const [status, setstatus] = useState("")
-  const [display, setdisplay] = useState("hidden")
+  const [status, setstatus] = useState("");
+  const [display, setdisplay] = useState("hidden");
 
   const handleSearch = async (event) => {
     setSearching(true);
@@ -35,36 +35,43 @@ const BookSearch = () => {
     try {
       const bookshelf = JSON.parse(localStorage.getItem("bookshelf")) || [];
       localStorage.setItem("bookshelf", JSON.stringify([...bookshelf, book]));
-      setstatus("green")
-      setdisplay("")
+      setstatus("green");
+      setdisplay("");
     } catch (error) {
-      setstatus("red") 
-      setdisplay("")
+      setstatus("red");
+      setdisplay("");
     }
     setTimeout(() => {
-      setdisplay("hidden")
+      setdisplay("hidden");
     }, 2000);
-
   };
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-xl font-bold py-5">Search for Books</h1>
-      <p className={`absolute ${display} top-10 lg:top-1 mx-auto ${status==="green"?"bg-green-400":"bg-red-400"} text-white py-1 px-2 text-sm rounded-md`}>{status==="green"?"Book added successfully":"Some error occured"}</p>
-      <input
-        type="text"
-        value={query}
-        onChange={handleSearch}
-        placeholder="Enter book name"
-        className="p-1 pl-2 border-2 border-blue-200 rounded-md outline-0 focus:border-blue-500"
-      />
-      <div className="book-cards flex flex-wrap justify-center m-5">
-        <p className="p-2">{searching === true ? "Searching..." : ""}</p>
-        {books.map((book) => (
-          <BookCard key={book.key} book={book} onAdd={addToBookshelf} />
-        ))}
+    <>
+      <p
+        className={`fixed ${display} top-2 mx-auto ${
+          status === "green" ? "bg-green-400" : "bg-red-400"
+        } text-white py-1 px-2 text-center text-sm rounded-md left-0 right-0 w-[20%]`}
+      >
+        {status === "green" ? "Book added successfully" : "Some error occured"}
+      </p>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-xl font-bold py-5 z-0">Search for Books</h1>
+        <input
+          type="text"
+          value={query}
+          onChange={handleSearch}
+          placeholder="Enter book name"
+          className="p-1 pl-2 border-2 border-blue-200 rounded-md outline-0 focus:border-blue-500"
+        />
+        <div className="book-cards flex flex-wrap justify-center m-5">
+          <p className="p-2">{searching === true ? "Searching..." : ""}</p>
+          {books.map((book) => (
+            <BookCard key={book.key} book={book} onAdd={addToBookshelf} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
